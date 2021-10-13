b89ed08b0365b256aa1bcb04ff49d6fb3496dfae
sandra-mutai <sandramuttai@gmail.com>
2021-10-13 14:00:25 +0300

    my commit

---
+++ djangoenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/student/Documents/Environments/Order_Management_System_using_Django_Framework/Order-management-system/djangoenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
